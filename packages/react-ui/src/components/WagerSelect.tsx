6dbe467ca34ca4bc6e126c212fd7a60de2dd92ac
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2025-04-04 23:30:22 +0200

    Add files via upload

---
+++ packages/react-ui/src/components/WagerSelect.tsx
@@ -0,0 +1,30 @@
+import { useGamba } from 'gamba-react-v2'
+import React from 'react'
+import { Select } from './Select'
+import { TokenValue } from './TokenValue'
+
+export interface WagerSelectProps {
+  options: number[]
+  value: number
+  onChange: (value: number) => void
+  className?: string
+}
+
+/**
+ * @deprecated Use WagerInput with "options" prop
+ */
+export function WagerSelect(props: WagerSelectProps) {
+  const gamba = useGamba()
+  return (
+    <Select
+      className={props.className}
+      options={props.options}
+      value={props.value}
+      onChange={props.onChange}
+      disabled={gamba.isPlaying}
+      label={(value) => (
+        <TokenValue amount={value} />
+      )}
+    />
+  )
+}
